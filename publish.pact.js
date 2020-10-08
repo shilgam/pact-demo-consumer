c783f90728912ef91f534cee72b6f222656c2fe1
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2020-10-08 10:07:02 +0300

    Move hard coded params to .env file and fix codestyle (#1)
    * Move parameters to env vars
    
    * Fix codestyle issues

--- publish.pact.js
+++ publish.pact.js
@@ -1,66 +1,63 @@
 const pact = require('@pact-foundation/pact-node');
 const path = require('path');
 
-
-if (!process.env.CI && !process.env.PUBLISH_PACT) {
-    console.log('skipping Pact publish...');
-    return
+if (!process.env.CI) {
+  console.log('skipping Pact publish...');
+  process.exit();
 }
 
-const pactBrokerUrl = process.env.PACT_BROKER_URL || 'http://localhost:8081';
-const pactBrokerUsername = process.env.PACT_BROKER_USERNAME || 'pact_workshop';
-const pactBrokerPassword = process.env.PACT_BROKER_PASSWORD || 'pact_workshop';
-const pactBrokerToken = process.env.PACT_BROKER_TOKEN;
+const pactBrokerUrl = process.env.LOCAL_PACT_BROKER ? process.env.LOCAL_PACT_BROKER_URL : process.env.PACT_BROKER_URL;
+const providerBaseUrl = process.env.PROVIDER_BASE_URL;
 const pactFilesOrDirs = [path.resolve(__dirname, './pacts/')];
 const gitCommitHash = require('child_process')
-    .execSync('git rev-parse --short HEAD')
-    .toString().trim();
+  .execSync('git rev-parse --short HEAD')
+  .toString().trim();
 
 const localGitBranch = require('child_process')
-    .execSync('git rev-parse --abbrev-ref HEAD')
-    .toString();
-
-const providerBaseUrl = 'http://localhost:8080';
+  .execSync('git rev-parse --abbrev-ref HEAD')
+  .toString();
 
 const gitBranch = process.env.TRAVIS_BRANCH || localGitBranch;
 
 let opts;
-if (process.env.PACT_BROKER_TOKEN) {
-    console.log('Publishing contract to remote Pact Broker...');
-    opts = {
-        providerBaseUrl,
-        pactFilesOrDirs,
-        pactBroker: pactBrokerUrl,
-        tags: [gitBranch],
-        consumerVersion: gitCommitHash,
-        pactBrokerToken,
-    };
+if (typeof process.env.LOCAL_PACT_BROKER === 'undefined' || process.env.LOCAL_PACT_BROKER !== 'true') {
+  console.log('Publishing contract to remote Pact Broker...');
+
+  opts = {
+    providerBaseUrl,
+    pactFilesOrDirs,
+    pactBroker: pactBrokerUrl,
+    tags: [gitBranch],
+    consumerVersion: gitCommitHash,
+    pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+  };
 } else {
-    console.log('Publishing contract to local Pact Broker...');
-    opts = {
-        providerBaseUrl,
-        pactFilesOrDirs,
-        pactBroker: pactBrokerUrl,
-        tags: [gitBranch],
-        consumerVersion: gitCommitHash,
-        pactBrokerUsername,
-        pactBrokerPassword,
-    };
+  console.log('Publishing contract to local Pact Broker...');
+
+  opts = {
+    providerBaseUrl,
+    pactFilesOrDirs,
+    pactBroker: pactBrokerUrl,
+    tags: [gitBranch],
+    consumerVersion: gitCommitHash,
+    pactBrokerUsername: process.env.LOCAL_PACT_BROKER_USERNAME,
+    pactBrokerPassword: process.env.LOCAL_PACT_BROKER_PASSWORD,
+  };
 }
 
 console.log('>>>>> opts:');
 console.log(opts);
 
 pact
-    .publishPacts(opts)
-    .then(() => {
-        console.log('Pact contract publishing complete!');
-        console.log('');
-        console.log(`Head over to ${pactBrokerUrl} and login with`);
-        console.log(`=> Username: ${pactBrokerUsername}`);
-        console.log(`=> Password: ${pactBrokerPassword}`);
-        console.log('to see your published contracts.');
-    })
-    .catch((e) => {
-        console.log('Pact contract publishing failed: ', e);
-    });
+  .publishPacts(opts)
+  .then(() => {
+    console.log('Pact contract publishing complete!');
+    console.log('');
+    console.log(`Head over to ${pactBrokerUrl} and login with`);
+    console.log(`=> Username: ${process.env.LOCAL_PACT_BROKER_USERNAME}`);
+    console.log(`=> Password: ${process.env.LOCAL_PACT_BROKER_PASSWORD}`);
+    console.log('to see your published contracts.');
+  })
+  .catch((e) => {
+    console.log('Pact contract publishing failed: ', e);
+  });
